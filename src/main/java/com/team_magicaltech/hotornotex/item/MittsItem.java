1f9e79f4094a514726aaf59173ef9592e7f6e7c0
joshu-TeamMagicalTech <100466285+joshu-TeamMagicalTech@users.noreply.github.com>
2022-05-30 12:26:03 +0900

    Update MittsItem.java

--- src/main/java/com/team_magicaltech/hotornotex/item/MittsItem.java
+++ src/main/java/com/team_magicaltech/hotornotex/item/MittsItem.java
@@ -28,6 +28,6 @@ public class MittsItem extends Item {
     @Override
     public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> tooltip, ITooltipFlag flagIn) {
         super.addInformation(stack, worldIn, tooltip, flagIn);
-        //tooltip.add(new TextComponentTranslation("item.hotornotex.mitts.tooltip").getUnformattedComponentText());
+        tooltip.add(new TextComponentTranslation("item.hotornotex.mitts.tooltip").getUnformattedComponentText());
     }
 }
